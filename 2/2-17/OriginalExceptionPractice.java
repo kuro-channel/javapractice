a84d8c491acb5a7321e11233d8ed08d1b2f89598
takumikuroiwa <takumikuroiwa@gmail.com>
2019-06-11 23:52:03 +0900

    6/11
    更新

--- 2/2-17/OriginalExceptionPractice.java
+++ 2/2-17/OriginalExceptionPractice.java
@@ -1,66 +1,65 @@
-// Copyright (c) 2018 Kenji Iida  All rights reserved.
 /*
 
-    �Ǝ���O�̖��
+    独自例外の問題
 
-    �����Ԃ�\��Car�N���X�ƁA�K�X����\��GasolineException
-    ��O�N���X���쐬���Ă��������B
+    自動車を表すCarクラスと、ガス欠を表すGasolineException
+    例外クラスを作成してください。
 
-    �܂��ACar�N���X���g�p����OriginalExceptionPractice�N���X��
-    main���\�b�h�͂��łɊ������Ă��܂��B�i�ύX���Ȃ��ł��������j
+    また、Carクラスを使用するOriginalExceptionPracticeクラスの
+    mainメソッドはすでに完成しています。（変更しないでください）
 
-    [ GasolineException��O�N���X ]
-    �P�D���̃N���X�͌�����O�Ȃ̂ŁAException�N���X���p�����܂��B
+    [ GasolineException例外クラス ]
+    １．このクラスは検査例外なので、Exceptionクラスを継承します。
 
-    �Q�DString�^�̈������P���R���X�g���N�^���`���܂��B
-        �X�[�p�[�N���X�ł���Exception�N���X�ɂ������V�O�l�`����
-        �R���X�g���N�^�����݂���̂ŁA�����Ŏ󂯎�����f�[�^��
-        �X�[�p�[�N���X�̃R���X�g���N�^�ɓn���܂��B
+    ２．String型の引数を１つ持つコンストラクタを定義します。
+        スーパークラスであるExceptionクラスにも同じシグネチャの
+        コンストラクタが存在するので、引数で受け取ったデータを
+        スーパークラスのコンストラクタに渡します。
 
-    [ Car�N���X ]
-    �P�DCar�N���X�ɂ͂R�̃C���X�^���X�t�B�[���h������܂��B
+    [ Carクラス ]
+    １．Carクラスには３つのインスタンスフィールドがあります。
 
-      �E�i���o�[��\��number�C���X�^���X�t�B�[���h�iint�^�j
-      �E�Ԏ��\��type�C���X�^���X�t�B�[���h�iString�^�j
-      �E�K�\�����ʂ�\��gas�C���X�^���X�t�B�[���h�idouble�^�j
+      ・ナンバーを表すnumberインスタンスフィールド（int型）
+      ・車種を表すtypeインスタンスフィールド（String型）
+      ・ガソリン量を表すgasインスタンスフィールド（double型）
 
-    �Q�D�������R�󂯎��R���X�g���N�^���`���܂��B
-        ���̎󂯎�����R�̒l�́A���̂܂܂R�̃C���X�^���X�t�B�[���h
-        �ɓn���܂��B(main���\�b�h�̋L�q���Q�l�ɂ��Ă�������)
+    ２．引数を３つ受け取るコンストラクタを定義します。
+        その受け取った３つの値は、そのまま３つのインスタンスフィールド
+        に渡します。(mainメソッドの記述を参考にしてください)
 
-    �R�D�Ԃ̑��s��\��run�C���X�^���X���\�b�h�i�����E�߂�l�Ȃ��j��
-        ��`���܂��Brun���\�b�h�̓K�X����\������GasolineException��O
-        ���X���[����\��������̂ŁAthrows�L�[���[�h��p���Ďw�肵�Ă��������B
-        ���\�b�h�̍ŏ��̏����Ƃ��āA�K�\�����ʂ��`�F�b�N���܂��B
-        �K�\�����ʂ�0.1���b�g�������̏ꍇ�́A�K�X����O�N���X���C���X�^���X�����A
-        �X���[���܂��B�C���X�^���X������Ƃ��A�G���[���b�Z�[�W���R���X�g���N�^��
-        �����Ƃ��ēn���܂��B�i�G���[���b�Z�[�W�͉��L���s���ʂ��Q�l�j
-        �K�\�����ʂ�0.1���b�g���ȏ�̏ꍇ�͑��s���b�Z�[�W��\�����A�K�\�����ʂ�
-        0.1���b�g�������炵�܂��B�i���s���b�Z�[�W�͉��L���s���ʂ��Q�l�j
+    ３．車の走行を表すrunインスタンスメソッド（引数・戻り値なし）を
+        定義します。runメソッドはガス欠を表現するGasolineException例外
+        をスローする可能性があるので、throwsキーワードを用いて指定してください。
+        メソッドの最初の処理として、ガソリン量をチェックします。
+        ガソリン量が0.1リットル未満の場合は、ガス欠例外クラスをインスタンス化し、
+        スローします。インスタンス化するとき、エラーメッセージをコンストラクタの
+        引数として渡します。（エラーメッセージは下記実行結果を参考）
+        ガソリン量が0.1リットル以上の場合は走行メッセージを表示し、ガソリン量を
+        0.1リットル分減らします。（走行メッセージは下記実行結果を参考）
 
-    �y���s���ʁz
-    �i���o�[123�̃��~�I�͑��s���Ă��܂��B
-    �i���o�[123�̃��~�I�͑��s���Ă��܂��B
-    �i���o�[123�̃��~�I�͑��s���Ă��܂��B
-    �i���o�[123�̃��~�I�͑��s���Ă��܂��B
-    �i���o�[123�̃��~�I�͑��s���Ă��܂��B
-    �i���o�[123�̃��~�I�͑��s���Ă��܂��B
-    �i���o�[123�̃��~�I�͑��s���Ă��܂��B
-    �i���o�[123�̃��~�I�͑��s���Ă��܂��B
-    �i���o�[123�̃��~�I�̓K�\�����s���̂��ߑ��s�ł��܂���B
+    【実行結果】
+    ナンバー123のロミオは走行しています。
+    ナンバー123のロミオは走行しています。
+    ナンバー123のロミオは走行しています。
+    ナンバー123のロミオは走行しています。
+    ナンバー123のロミオは走行しています。
+    ナンバー123のロミオは走行しています。
+    ナンバー123のロミオは走行しています。
+    ナンバー123のロミオは走行しています。
+    ナンバー123のロミオはガソリン不足のため走行できません。
 
 */
 public class OriginalExceptionPractice{
 
     public static void main(String[] args){
 
-        // �����ԃI�u�W�F�N�g�̐���
-        Car mycar = new Car(123, "���~�I", 0.8);
+        // 自動車オブジェクトの生成
+        Cars mycar = new Cars(123, "ロミオ", 0.8);
 
-        // �K�\�������Ȃ��Ȃ�܂ő��s����
+        // ガソリンがなくなるまで走行する
         try{
             while(true){
-                // ���s����
+                // 走行する
                 mycar.run();
             }
         }
@@ -72,7 +71,37 @@ public class OriginalExceptionPractice{
 
 }
 
-// �����ɎԃN���X���쐬���Ă�������
+// ここに車クラスを作成してください
+class Cars {
+	// ナンバー
+	public int number;
+	// 車種
+	public String type;
+	// ガソリン
+	public double gas;
+	// コンストラクタ
+	public Cars(int number, String type, double gas) {
+		this.number = number;
+		this.type = type;
+		this.gas = gas;
+	}
 
-// �����ɃK�X����O�N���X���쐬���Ă�������
+	// 走行する
+	public void run() throws GasolineException {
+		System.out.println("ナンバー" + this.number + "の" + this.type + "は走行しています。");
 
+		if(gas < 0.1) {
+			throw new GasolineException("ナンバー" + this.number + "の" + this.type + "はガソリン不足のため走行できません。");
+		}
+		// ガソリンを0.1リットル減らす
+		gas -=0.1;
+	}
+}
+
+// ここにガス欠例外クラスを作成してください
+class GasolineException extends  Exception {
+	// コンストラクタ
+	public GasolineException(String message) {
+		super(message);
+	}
+}
